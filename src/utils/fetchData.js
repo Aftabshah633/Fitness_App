0621600749468ab5c074d6b4596253b94f6ff22a
Syed Aftab <59339882+Aftabshah633@users.noreply.github.com>
2025-03-07 11:50:27 +0000

    search exercises , exercise

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "05e441ba4dmsh99bd244bdfc28d4p1c002bjsnf39a0ef4cae2",
   },
 };
 
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
+    "X-RapidAPI-Key": "05e441ba4dmsh99bd244bdfc28d4p1c002bjsnf39a0ef4cae2",
   },
 };
 
